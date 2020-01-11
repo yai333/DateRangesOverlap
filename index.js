db1601933b247b39c8f860cb6d7105d7d8048666
YI AI <yi.ai@neaminational.org.au>
2020-01-11 22:42:13 +1100

    add error catch to main function

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const OUTPUTFILE = `${__dirname}/output/output.txt`;
 const APPOINTMENT_INPUTFILE = `${__dirname}/input/sample.txt`;
 
 if (require.main === module) {
-  processTimeConflictCheck(APPOINTMENT_INPUTFILE, OUTPUTFILE).then(() => {
-    console.log(
-      `Appointment datetime validating finished.` +
-        ` Please check ${OUTPUTFILE} file for details`
-    );
-  });
+  processTimeConflictCheck(APPOINTMENT_INPUTFILE, OUTPUTFILE)
+    .then(() => {
+      console.log(
+        `Appointment datetime validating finished.` +
+          ` Please check ${OUTPUTFILE} file for details`
+      );
+    })
+    .catch(e => throw e);
 }
